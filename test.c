1bf23e65069fb16857541458151d37af76fcf742
VinayakNPN <chouhanvinayak86@gmail.com>
2025-08-24 16:56:39 +0530

    Test C file loaded

---
+++ test.c
@@ -0,0 +1,24 @@
+#include<vincen.h>
+void main() {
+    printf("%d\n",Factorial(5));
+
+    printf("%d\n",Prime(6));
+
+    printf("%d\n",Reverse(1456));
+
+    printf("%d\n",CountDigit(1456));
+
+    printf("%d\n",ArmStrong(153));
+
+    printf("%d\n",Palindrome(1231));
+
+    PrintFibonacci(7);
+
+    printf("%d\n",Strong(123));
+
+    printf("%d\n",Perfect(6));
+
+    printf("%d\n",SumDigits(6411));
+    
+    printf("%d\n",ProductDigits(6411));
+}
